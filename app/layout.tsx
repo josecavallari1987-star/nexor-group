1de05ff663f3a1b5ddd3f3c506d0dc880e636d07
Jose Cavallari <josecavallari1987@gmail.com>
2026-05-28 19:34:38 -0300

    fix layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,16 @@
  export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
-        
+    <html lang="es">
+      <body>
         <header>
           <img src="/logo.png" alt="Grupo Nexor" width={150} />
         </header>
 
         {children}
-
       </body>
     </html>
   );
